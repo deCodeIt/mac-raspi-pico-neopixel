d4e2ac91dff26caa679e0c8c5a168e7f147d4812
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-03-26 23:09:43 +0530

    Test code works

--- src/main.ts
+++ src/main.ts
@@ -123,7 +123,8 @@ const processPixels = async ( socket: net.Socket, pixel: number[][][] ) => {
 const encodeLedPixels = ( socket: net.Socket, pixel: number[][] ) => {
   let msg = 'deaddeed'; // hex init
   for( let i = 0; i < pixel.length; i++ ) {
-    msg += `${pixel[ i ][ 0 ].toString( 16 )}${pixel[ i ][ 1 ].toString( 16 )}${pixel[ i ][ 2 ].toString( 16 )}`;
+    // msg += `${pixel[ i ][ 0 ].toString( 16 )}${pixel[ i ][ 1 ].toString( 16 )}${pixel[ i ][ 2 ].toString( 16 )}`;
+    msg += '00ff00'; // Red
   }
   msg += 'feedfeed';
   console.log( 'Msg', msg );
