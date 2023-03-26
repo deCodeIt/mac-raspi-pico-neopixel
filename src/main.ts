ce546f9f7154224abcd4779c0d2f810ab3ea086c
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-03-26 16:55:01 +0530

    Get average color fr strips

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ const numLedsRight = 65;
 const ss = robot.screen.capture();
 const width = 128;
 const height = Math.floor( width * ss.height / ss.width );
+const topEnd = Math.floor( height / 3 );
+const leftEnd = Math.floor( width / 3 );;
+const rightStart = Math.floor( width * 2 / 3 );
 
 // Capture a screenshot every second and process the grids
 const generateNewValue = async () => {
@@ -31,33 +34,63 @@ const generateNewValue = async () => {
     imgPixels[ x ][ y ] = [ this.bitmap.data[ idx + 0 ], this.bitmap.data[ idx + 1 ], this.bitmap.data[ idx + 2 ], this.bitmap.data[ idx + 3 ] ];
   } );
   // console.log( 'ImgPixels', imgPixels );
+  return processPixels( imgPixels );
 };
 
-const processPixels = async ( pixel: number[][][], width: number, height: number ) => {
+const processPixels = async ( pixel: number[][][] ) => {
   // leftStrip
   const leftStrip = new Array( numLedsLeft );
+  const heightPerLeftLed = height / numLedsLeft;
   for( let i = 0; i < numLedsLeft; i++ ) {
-    // leftStrip[ i ] = getAverageColor(
-      
-    // );
+    leftStrip[ i ] = getAverageColor(
+      pixel,
+      0,
+      heightPerLeftLed * i,
+      leftEnd,
+      heightPerLeftLed * ( i + 1 )
+    );
+  }
+  // topStrip
+  const topStrip = new Array( numLedsTop );
+  const widthPerTopLed = height / numLedsTop;
+  for( let i = 0; i < numLedsTop; i++ ) {
+    topStrip[ i ] = getAverageColor(
+      pixel,
+      widthPerTopLed * i,
+      0,
+      widthPerTopLed * ( i + 1 ),
+      topEnd
+    );
+  }
+  // rightStrip
+  const rightStrip = new Array( numLedsRight );
+  const heightPerRightLed = height / numLedsRight;
+  for( let i = 0; i < numLedsRight; i++ ) {
+    rightStrip[ i ] = getAverageColor(
+      pixel,
+      rightStart,
+      heightPerRightLed * i,
+      width,
+      heightPerRightLed * ( i + 1 )
+    );
   }
 }
 
 // Helper function to get the average color of a grid cell
-const getAverageColor = ( pixel: number[][][], x: number, y: number, width: number, height: number): number[] => {
+const getAverageColor = ( pixel: number[][][], x: number, y: number, xEmd: number, yEnd: number): number[] => {
   let r = 0;
   let g = 0;
   let b = 0;
 
-  for( let i = 0; i < width - x; i++ ) {
-    for( let j = 0; j < height - y; j++ ) {
+  for( let i = 0; i < xEmd - x; i++ ) {
+    for( let j = 0; j < yEnd - y; j++ ) {
       r += pixel[i][j][0];
       g += pixel[i][j][1];
       b += pixel[i][j][2];
     }
   }
 
-  const numPixels = ( width - x ) * ( height - y );
+  const numPixels = ( xEmd - x ) * ( yEnd - y );
   return [Math.round(r / numPixels), Math.round(g / numPixels), Math.round(b / numPixels)];
 };
 
