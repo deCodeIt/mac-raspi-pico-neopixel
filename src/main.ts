7d9dcf580ebaf4f71d1d5173fa0141ffcffe8213
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-03-26 13:11:21 +0530

    Unused vars ignored

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,15 @@
-import screenshot from 'screenshot-desktop';
-import Jimp from 'jimp';
+import * as robot from "robotjs";
 
-// Set the grid dimensions
-// const leftRows = 67;
-// const topColumns = 110;
-// const rightRows = 65;
+const NUM_SCREENSHOTS = 100;
 
-// Capture a screenshot every second and process the grids
-const generateNewValue = async () => {
-  const img = await screenshot( { format: 'png' } );
-  const jimpImg = (await Jimp.read( img )).resize( 256, 256 );
-  const height = jimpImg.getHeight();
-  const width = jimpImg.getWidth();
+const start = process.hrtime();
+for (let i = 0; i < NUM_SCREENSHOTS; i++) {
+  const screenShot = robot.screen.capture(0, 0, robot.getScreenSize().width, robot.getScreenSize().height);
+}
+const end = process.hrtime(start);
 
-  // Convert Image to Pixels.
-  const imgPixels: number[][][] = new Array( width );
-  jimpImg.scan( 0, 0, width, height, function ( x, y, idx ) {
-    // console.log( `(${x}, ${y}), ${idx}` );
-    if( y === 0 ) {
-      // Initialize array for new column.
-      imgPixels[ x ] = new Array( height );
-    }
-    // console.log( `(${x}, ${y}), ${idx} => ${imgPixels[ x ][ y ]}` );
-    imgPixels[ x ][ y ] = [ this.bitmap.data[ idx + 0 ], this.bitmap.data[ idx + 1 ], this.bitmap.data[ idx + 2 ], this.bitmap.data[ idx + 3 ] ];
-  } );
-  // console.log( 'ImgPixels', imgPixels );
-};
+const timeInSeconds = end[0] + end[1] / 1000000000;
+const screenshotsPerSecond = NUM_SCREENSHOTS / timeInSeconds;
 
-// Helper function to get the average color of a grid cell
-// const getAverageColor = (pixels: number[]): number[] => {
-//   let r = 0;
-//   let g = 0;
-//   let b = 0;
-
-//   for (let i = 0; i < pixels.length; i += 4) {
-//     r += pixels[i];
-//     g += pixels[i + 1];
-//     b += pixels[i + 2];
-//   }
-
-//   const numPixels = pixels.length / 4;
-//   return [Math.round(r / numPixels), Math.round(g / numPixels), Math.round(b / numPixels)];
-// };
-
-const main = async () => {
-  const start = new Date().getTime();
-  let count = 0;
-  while( true ) {
-    count++;
-    const elaspedTime = ( new Date().getTime() - start ) / 1000;
-    if( elaspedTime > 0 ) {
-      console.log( `Iterations / sec:  ${ Math.floor( count / elaspedTime ) }` )
-    }
-    await generateNewValue();
-  }
-};
-
-main().finally( () => console.log( 'Done!' ) );
+console.log(`Took ${timeInSeconds} seconds to take ${NUM_SCREENSHOTS} screenshots`);
+console.log(`Average screenshots per second: ${screenshotsPerSecond.toFixed(2)}`);
