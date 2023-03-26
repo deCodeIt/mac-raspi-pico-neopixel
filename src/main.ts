01e9206052f6ea87d4d68a4bc4a3b5b8df29d152
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-03-26 12:54:34 +0530

    New way to fetch pizel info beforehand and also benchmark

--- src/main.ts
+++ src/main.ts
@@ -1,106 +1,59 @@
-import { Readable } from "stream";
 import screenshot from 'screenshot-desktop';
-import getPixels from 'get-pixels';
-import probe from 'probe-image-size';
 import Jimp from 'jimp';
 
 // Set the grid dimensions
-const leftRows = 67;
-const topColumns = 110;
-const rightRows = 65;
-const rightBoundary = 2 / 3;
+// const leftRows = 67;
+// const topColumns = 110;
+// const rightRows = 65;
 
 // Capture a screenshot every second and process the grids
-setInterval(() => {
-  screenshot().then( ( img ) => {
-    probe( Readable.from( img ) ).then(({ width, height }) => {
-      Jimp.read(img).then((jimpImg) => {
-        const leftGrid = jimpImg.crop(
-          0,
-          0,
-          Math.floor(width * rightBoundary),
-          height
-        );
-        const topGrid = jimpImg.crop(
-          Math.floor(width * (1 - rightBoundary) / 2),
-          0,
-          Math.floor(width * rightBoundary),
-          Math.floor(height / 3)
-        );
-        const rightGrid = jimpImg.crop(
-          Math.floor(width * (1 - rightBoundary)),
-          0,
-          Math.floor(width * rightBoundary),
-          height
-        );
+const generateNewValue = async () => {
+  const img = await screenshot( { format: 'png' } );
+  const jimpImg = (await Jimp.read( img )).resize( 256, 256 );
+  const height = jimpImg.getHeight();
+  const width = jimpImg.getWidth();
 
-        getGridColors(leftGrid, leftRows, (leftArr: number[][]) => {
-          getGridColors(topGrid, topColumns, (topArr: number[][]) => {
-            getGridColors(rightGrid, rightRows, (rightArr: number[][]) => {
-              const mergedArr = [...leftArr, ...topArr, ...rightArr];
-              console.log(mergedArr);
-            });
-          });
-        });
-      });
-    });
-  }).catch((err) => {
-    console.error(err);
-  });
-}, 1000);
-
-// Get the aggregate color of each cell in a grid
-const getGridColors = (
-  grid: Jimp,
-  cells: number,
-  callback: (arr: number[][]) => void
-): void => {
-  grid.getBuffer(Jimp.MIME_PNG, (err: any, buffer: Buffer) => {
-    if (err) {
-      console.error(err);
-      return;
+  // Convert Image to Pixels.
+  const imgPixels: number[][][] = new Array( width );
+  jimpImg.scan( 0, 0, width, height, function ( x, y, idx ) {
+    // console.log( `(${x}, ${y}), ${idx}` );
+    if( y === 0 ) {
+      // Initialize array for new column.
+      imgPixels[ x ] = new Array( height );
     }
-
-    getPixels(buffer, Jimp.MIME_PNG, (err: any, pixels: any) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      const arr: number[][] = [];
-
-      for (let i = 0; i < cells; i++) {
-        const start = Math.floor((i / cells) * (grid.bitmap.height - 1));
-        const end = Math.floor(((i + 1) / cells) * (grid.bitmap.height - 1));
-        const cellPixels = pixels.pick(
-          null,
-          null,
-          null,
-          null,
-          { x: 0, y: start },
-          { x: grid.bitmap.width - 1, y: end }
-        ).data;
-        const avg = getAverageColor(cellPixels);
-        arr.push(avg);
-      }
-
-      callback(arr);
-    });
-  });
+    // console.log( `(${x}, ${y}), ${idx} => ${imgPixels[ x ][ y ]}` );
+    imgPixels[ x ][ y ] = [ this.bitmap.data[ idx + 0 ], this.bitmap.data[ idx + 1 ], this.bitmap.data[ idx + 2 ], this.bitmap.data[ idx + 3 ] ];
+  } );
+  // console.log( 'ImgPixels', imgPixels );
 };
 
 // Helper function to get the average color of a grid cell
-const getAverageColor = (pixels: number[]): number[] => {
-  let r = 0;
-  let g = 0;
-  let b = 0;
+// const getAverageColor = (pixels: number[]): number[] => {
+//   let r = 0;
+//   let g = 0;
+//   let b = 0;
 
-  for (let i = 0; i < pixels.length; i += 4) {
-    r += pixels[i];
-    g += pixels[i + 1];
-    b += pixels[i + 2];
+//   for (let i = 0; i < pixels.length; i += 4) {
+//     r += pixels[i];
+//     g += pixels[i + 1];
+//     b += pixels[i + 2];
+//   }
+
+//   const numPixels = pixels.length / 4;
+//   return [Math.round(r / numPixels), Math.round(g / numPixels), Math.round(b / numPixels)];
+// };
+
+const main = async () => {
+  const start = new Date().getTime();
+  let count = 0;
+  while( true ) {
+    count++;
+    const elaspedTime = ( new Date().getTime() - start ) / 1000;
+    if( elaspedTime > 0 ) {
+      console.log( `Iterations / sec:  ${ Math.floor( count / elaspedTime ) }` )
+    }
+    await generateNewValue();
   }
-
-  const numPixels = pixels.length / 4;
-  return [Math.round(r / numPixels), Math.round(g / numPixels), Math.round(b / numPixels)];
 };
+
+main().finally( () => console.log( 'Done!' ) );
