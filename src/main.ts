89548e87cba5d7b283082b37b68f6ae73dd0dff2
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-08-20 08:17:02 +0530

    Continuously generate value

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,9 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     console.log(`Received data: ${data}`);
     if( [ 'CONNECTED', 'DISPLAYED' ].includes ( data.toString() ) ) {
-      generateNewValue( socket );
+      while( true ) {
+        generateNewValue( socket );
+      }
     }
   });
 
